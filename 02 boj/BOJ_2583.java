4d4b3250d8080f3eca7b8965d4b9204e476efe88
Jason <insu0929@naver.com>
2022-01-27 23:46:25 +0900

    BOJ 2583
    https://www.acmicpc.net/problem/2583

---
+++ 02 boj/BOJ_2583.java
@@ -0,0 +1,38 @@
+/*
+ * @(#)BOJ_2583.java $version 2022/01/27
+ *
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * @author Insu Yang
+ */
+public class BOJ_2583 {
+
+    static int[][] map;
+    static boolean[][] visited;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] dim = br.readLine().split(" ");
+
+        int col = Integer.parseInt(dim[0]);
+        int row = Integer.parseInt(dim[1]);
+
+        int k = Integer.parseInt(dim[2]);
+
+        int[][] rects = new int[k][2];
+
+        for(int i = 0 ; i < k; i++){
+            String[] rectCoor = br.readLine().split(" ");
+
+
+        }
+
+    }
+}
