bec6d1592d019e817f2c07c072ea2b5ce4bc6fac
Jason <insu0929@naver.com>
2022-02-03 23:40:25 +0900

    BOJ 2638
    start

---
+++ 02 boj/BOJ_2638.java
@@ -0,0 +1,24 @@
+/*
+ * @(#)BOJ_2638.java $version 2022/02/03
+ *
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * @author Insu Yang
+ */
+public class BOJ_2638 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] input = br.readLine().split(" ");
+        int n = Integer.parseInt(input[0]);
+        int m = Integer.parseInt(input[1]);
+
+
+    }
+}
