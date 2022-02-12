4962bf5dd36fbf942d253ed397cb9b61ed8669a0
Jason <insu0929@naver.com>
2022-02-12 23:49:24 +0900

    BOJ 4673
    https://www.acmicpc.net/problem/4673

---
+++ 02 boj/BOJ_4673.java
@@ -0,0 +1,25 @@
+/*
+ * @(#)BOJ_4673.java $version 2022/02/12
+ *
+ */
+
+import java.util.HashSet;
+
+/**
+ * @author Insu Yang
+ */
+public class BOJ_4673 {
+    public static void main(String[] args){
+        HashSet<Integer> list = new HashSet<>();
+
+        for(int i = 0 ; i < 10000; i++){
+
+        }
+
+    }
+
+    public int boolean isSelfNo(int n){
+
+
+    }
+}
