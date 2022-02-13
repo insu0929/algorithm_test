1865feacf709a562dbcc0dfcee318d8caa5bda6f
Jason <insu0929@naver.com>
2022-02-13 23:57:07 +0900

    BOJ 4673
    https://www.acmicpc.net/problem/4673

--- 02 boj/BOJ_4673.java
+++ 02 boj/BOJ_4673.java
@@ -3,6 +3,7 @@
  *
  */
 
+import java.util.ArrayList;
 import java.util.HashSet;
 
 /**
@@ -12,14 +13,29 @@ public class BOJ_4673 {
     public static void main(String[] args){
         HashSet<Integer> list = new HashSet<>();
 
-        for(int i = 0 ; i < 10000; i++){
+        for(int i = 1 ; i <= 10000; i++){
+            int n = i;
+            int dn = 0;
 
+            while(n > 0) {
+                dn += n%10;
+                n /= 10;
+            }
+
+            dn = dn + i;
+            list.add(dn);
         }
 
-    }
+        ArrayList<Integer> answer = new ArrayList<>();
 
-    public int boolean isSelfNo(int n){
+        for(int i = 1; i <= 10000; i++){
+            if(!list.contains(i))
+                answer.add(i);
+        }
 
+        for(int no : answer){
+            System.out.println(no + " ");
+        }
 
     }
 }
