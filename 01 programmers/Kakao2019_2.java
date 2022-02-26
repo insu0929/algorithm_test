b03169f16cb64e8cfef876aae2bd904abe911744
Jason <insu0929@naver.com>
2022-02-26 23:55:39 +0900

    2019 카카오 블라인드: 실패율
    https://programmers.co.kr/learn/courses/30/lessons/42889

--- 01 programmers/Kakao2019_2.java
+++ 01 programmers/Kakao2019_2.java
@@ -21,34 +21,42 @@ public class Kakao2019_2 {
     }
 
     public static int[] solution(int N, int[] stages){
-        int[] answer = new int[N];
 
-        List<int[]> list = new ArrayList<>();
-
-        for(int i = 0 ; i < N; i++){
-            list.add(new int[]{i, 0});
-        }
+        int[] stuck = new int[N+2];
+        int[] passed = new int[N+1];
 
         for(int i = 0; i < stages.length; i++){
             int userLvl = stages[i];
-            int[] stage = list.get(userLvl);
-            stage[1]++;
-            list.set(userLvl, stage);
+
+            stuck[userLvl]++;
+
+            for(int j = 1; j < userLvl; j++){
+                passed[j]++;
+            }
         }
 
-        list.sort(new Comparator<int[]>() {
+        List<double[]> failRate = new ArrayList<>();
+
+        for(int i = 1; i <= N; i++){
+            double fail = (double)stuck[i] / (double)passed[i];
+            failRate.add(new double[]{i, fail});
+        }
+
+        failRate.sort(new Comparator<double[]>() {
             @Override
-            public int compare(int[] o1, int[] o2) {
+            public int compare(double[] o1, double[] o2) {
                 if(o1[1] > o2[1]) return -1;
                 else if(o1[1] < o2[1]) return 1;
                 else{
-                    return o1[0] - o2[0];
+                    return (int)(o1[0] - o2[1]);
                 }
             }
         });
 
-        for(int i = 0 ; i < N; i++){
-            answer[i] = list.get(i)[0];
+        int[] answer = new int[N];
+
+        for(int i = 1 ; i <= N; i++){
+            answer[i-1] = (int)failRate.get(i)[0];
         }
 
         return answer;
