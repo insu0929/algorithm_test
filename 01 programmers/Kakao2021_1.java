bfdd4b6a7823f8ff0791cf7d4c451b7691b608ca
Jason <insu0929@naver.com>
2022-03-07 23:45:03 +0900

    2021: 카카오 블라인드

--- 01 programmers/Kakao2021_1.java
+++ 01 programmers/Kakao2021_1.java
@@ -4,8 +4,19 @@
  */
 package com.jason.argos;
 
+import java.util.ArrayList;
+import java.util.Map;
+
 /**
  * @author Insu Yang
  */
 public class Kakao2021_1 {
+
+    static Map<String, ArrayList<Integer>> allCases;
+
+    public static int[] solution(String[] info, String[] query){
+        int[] answer = new int[query.length];
+
+        
+    }
 }
