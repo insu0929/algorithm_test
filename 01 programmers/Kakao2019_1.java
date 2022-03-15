cb5da2b661da9f430f61f24ba0cdc9f5a8400fa9
Jason <insu0929@naver.com>
2022-03-15 23:40:51 +0900

    2019 카카오 블라인드: 후보키

---
+++ 01 programmers/Kakao2019_1.java
@@ -0,0 +1,23 @@
+/*
+ * @(#)Kakao2019_1.java $version 2022/03/11
+ *
+ */
+package com.jason.argos;
+
+/**
+ * @author Insu Yang
+ */
+public class Kakao2019_1 {
+    public static void main(String[] args){
+
+    }
+
+    public static int solution(String[][] relation){
+        int answer = 0;
+
+
+
+
+        return answer;
+    }
+}
