2d641be3968f9ec0a2ebaba9a8e85f1b3a302bb7
Jason <insu0929@naver.com>
2022-03-16 23:48:39 +0900

    2021 카카오 인턴 : 거리두기

---
+++ 01 programmers/Kakao2021i_01.java
@@ -0,0 +1,18 @@
+/*
+ * @(#)Kakao2021i_01.java $version 2022/03/16
+ *
+ */
+package com.jason.argos;
+
+/**
+ * @author Insu Yang
+ */
+public class Kakao2021i_01 {
+    public static void main(String[] args){
+
+    }
+
+    public int[] solution(String[][] places){
+
+    }
+}
