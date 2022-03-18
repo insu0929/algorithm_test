b4944e13ee5d66fe8fea613ee2070a0320038471
Jason <insu0929@naver.com>
2022-03-18 23:52:28 +0900

    후보키(1)

--- 01 programmers/Kakao2019_1.java
+++ 01 programmers/Kakao2019_1.java
@@ -4,10 +4,17 @@
  */
 package com.jason.argos;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
 /**
  * @author Insu Yang
  */
 public class Kakao2019_1 {
+    static List<HashSet<Integer>> candidateKey;
+    static String[][] relation;
+
     public static void main(String[] args){
 
     }
@@ -15,9 +22,19 @@ public class Kakao2019_1 {
     public static int solution(String[][] relation){
         int answer = 0;
 
+        candidateKey = new ArrayList<>();
+        Kakao2019_1.relation = relation;
 
+        int colLen = relation[0].length;
 
+        for(int i = 1 ; i <= colLen; ++i){
+        }
 
         return answer;
+    }
+
+    static void makeKeyComb(int attr, int maxAttr, int idx, int size, HashSet<Integer> keySet){
+
+
     }
 }
