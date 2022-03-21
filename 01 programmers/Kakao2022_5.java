e234b2c58e3195a00a569eb6011d5fb18a228abd
Jason <insu0929@naver.com>
2022-03-21 23:36:42 +0900

    2022 카카오 블라인드: 양과 늑대
    start

---
+++ 01 programmers/Kakao2022_5.java
@@ -0,0 +1,25 @@
+/*
+ * @(#)Kakao2022_5.java $version 2022/02/25
+ *
+ */
+package com.jason.argos;
+
+/**
+ * @author Insu Yang
+ */
+public class Kakao2022_5 {
+
+    public static void main(String[] args){
+
+    }
+
+    public static int solution(int[] info, int[][] edges){
+        int answer = 0;
+
+
+
+
+        return answer;
+
+    }
+}
