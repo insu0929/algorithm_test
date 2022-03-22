670cb19c7925ec1ceb8432faa4446c37c2aabe42
Jason <insu0929@naver.com>
2022-03-22 23:50:49 +0900

    양과늑대 1

--- 01 programmers/Kakao2022_5.java
+++ 01 programmers/Kakao2022_5.java
@@ -4,10 +4,17 @@
  */
 package com.jason.argos;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author Insu Yang
  */
 public class Kakao2022_5 {
+
+    static int[] type;
+    static List<Integer>[] adj;
+    static int maxCount;
 
     public static void main(String[] args){
 
@@ -15,11 +22,26 @@ public class Kakao2022_5 {
 
     public static int solution(int[] info, int[][] edges){
         int answer = 0;
+        type = info;
+        adj = new List[info.length];
 
+        for(int i = 0 ; i < info.length; i++){
+            adj[i] = new ArrayList<>();
+        }
+
+        for(int i = 0 ; i < edges.length; i++){
+            int parent = edges[i][0];
+            int child = edges[i][1];
+
+            adj[parent].add(child);
+        }
 
 
 
         return answer;
-
     }
+
+    static void dfs()
+
+
 }
