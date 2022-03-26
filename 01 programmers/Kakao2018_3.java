de0e4104c02c25d1bcfba7ae78e855b406b6da6f
Jason <insu0929@naver.com>
2022-03-26 23:56:43 +0900

    2018 카카오블라인드: 추석 트래픽

---
+++ 01 programmers/Kakao2018_3.java
@@ -0,0 +1,19 @@
+/*
+ * @(#)Kakao2018_3.java $version 2022/03/25
+ *
+ */
+package com.jason.argos;
+
+/**
+ * @author Insu Yang
+ */
+public class Kakao2018_3 {
+    public static void main(String[] args){
+
+    }
+
+    public static int solution(String[] lines){
+        int answer = 0;
+        return answer;
+    }
+}
